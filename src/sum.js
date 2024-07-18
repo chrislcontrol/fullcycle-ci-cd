12e2a781a7f45486edfeaed068e049a384cc5b40
Christian Silva <chrislcontrol@hotmail.com>
2024-07-17 23:59:42 -0300

    feat: new methods

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,18 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-module.exports = sum;
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  if (b === 0) throw new Error("Division by zero is not allowed");
+  return a / b;
+}
+
+module.exports = { sum, sub, mult, div };
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,7 +1,14 @@
-const sum = require("../src/sum");
+const {sum, sub} = require("../src/sum");
 
 test("Test if 1 + 1 is 2", () => {
     expect(
         sum(1, 1)
     ).toBe(2)
+})
+
+
+test("Test if 1 - 1 is 0", () => {
+    expect(
+        sub(1, 1)
+    ).toBe(0)
 })
