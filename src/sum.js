9685819e06aad3b8d8aca7071e6efd9a6e2a526b
Christian Silva <chrislcontrol@hotmail.com>
2024-07-18 00:08:42 -0300

    test: add new tests

--- src/sum.js
+++ src/sum.js
@@ -6,13 +6,15 @@ function sub(a, b) {
   return a - b;
 }
 
-function mult(a, b) {
+function times(a, b) {
   return a * b;
 }
 
 function div(a, b) {
-  if (b === 0) throw new Error("Division by zero is not allowed");
+  if (b === 0) {
+    throw new Error("Division by zero error");
+  }
   return a / b;
 }
 
-module.exports = { sum, sub, mult, div };
+module.exports = { sum, sub, times, div };
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, sub} = require("../src/sum");
+const {sum, sub, times, div} = require("../src/sum");
 
 test("Test if 1 + 1 is 2", () => {
     expect(
@@ -11,4 +11,25 @@ test("Test if 1 - 1 is 0", () => {
     expect(
         sub(1, 1)
     ).toBe(0)
+})
+
+
+test("Test if 2 * 5 is 10", () => {
+    expect(
+        times(2, 5)
+    ).toBe(10)
+})
+
+
+test("Test if 10 / 2 is 5", () => {
+    expect(
+        div(10, 2)
+    ).toBe(5)
+})
+
+
+test("Test if div by 0 throws error", () => {
+    expect(
+        () => {div(1, 0)}
+    ).toThrow("Division by zero error")
 })
